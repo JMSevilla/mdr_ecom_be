70a21b475ab19414d1f31a92cb8663fa4c2f6e16
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2022-08-18 09:05:35 +0800

    MDR40-022 UI and Server Scripting Validation

--- restful_api/utils/helper.py
+++ restful_api/utils/helper.py
@@ -1,8 +1,11 @@
-from restful_api.models import BusinessOwner
+import string
+import random
+from restful_api.models import Administrator, BusinessOwner, Student, Tokenization
 from restful_api.models import Project
 from django.contrib.auth.hashers import make_password
 from restful_api.models import AccountVerification_1
 from django.db.models import F
+from django.contrib.auth.hashers import check_password
 
 
 class GeneralParams:
@@ -17,6 +20,13 @@ class GeneralParams:
     field_verified_str = ''
     field_fetching_project = []
     field_fetching_bo = []
+    field_login_findUser = []
+    field_login_token = []
+    field_login_afterserializer = ''
+    field_login_lastId = 0
+    field_token_tokenresult = []
+    field_token_rebase = []
+
 
 class GeneralHelper:
     def Slug(method, condition, params):
@@ -34,6 +44,10 @@ class GeneralHelper:
                     return GeneralHelper.__init__findAllProjectByEmail(params)
                 elif condition == 'fetch-bo':
                     return GeneralHelper.__init__findAllBOByEmail(params)
+                elif condition == 'api/check-token-identify':
+                    return GeneralHelper.__init__tokenidentify(params)
+                elif condition == 'api/get-users-info':
+                    return GeneralHelper.__init__getcurrent_user(params)
             case 'POST':
                 if condition == 'api/business-owner-registration':
                     return GeneralHelper.__init__registration_businessowner(params)
@@ -41,11 +55,21 @@ class GeneralHelper:
                     return GeneralHelper.__init__project_entry(params)
                 elif condition == 'business-verification-dbentry':
                     return GeneralHelper.__init__verification_entry(params)
+                elif condition == 'login':
+                    return GeneralHelper.__init__findUsers(params)
+                elif condition == 'api/gettoken':
+                    return GeneralHelper.__init__checktoken(params)
             case 'PUT':
                 if condition == 'api/business-update-counts':
                     return GeneralHelper.__init__update_counts(params)
                 elif condition == 'api/business-update-to-verified':
                     return GeneralHelper.__init__update_to_verified(params)
+
+    def TokenSlug(method, condition, snapshot):
+        match method:
+            case 'POST':
+                if condition == 'tokenQueryBuild':
+                    return QueryBuilder.tokenizationQueryBuild(snapshot)
 
     # entity functions
 
@@ -155,10 +179,79 @@ class GeneralHelper:
         ).values()
         GeneralParams.field_fetching_project = filtered
         return GeneralParams.field_fetching_project
-    
+
     def __init__findAllBOByEmail(params):
         filtered = BusinessOwner.objects.filter(
             email=params
         ).values()
         GeneralParams.field_fetching_bo = filtered
         return GeneralParams.field_fetching_bo
+
+    def __init__findUsers(params):
+        if params['credential_type'] == 'bo':
+            bo_filtered = BusinessOwner.objects.filter(
+                email=params['email']
+            ).values()
+            GeneralParams.field_login_findUser = bo_filtered
+            return GeneralParams.field_login_findUser
+        return GeneralParams.field_login_findUser
+
+    def __init__checktoken(params):
+        token_list = Tokenization.objects.filter(
+            userID=params
+        ).values()
+        GeneralParams.field_login_token = token_list
+        return GeneralParams.field_login_token
+
+    def __init__tokenidentify(params):
+        tokenize = Tokenization.objects.filter(
+            userID=params
+        ).values()
+        GeneralParams.field_token_tokenresult = tokenize
+        return GeneralParams.field_token_tokenresult
+
+    def __init__getcurrent_user(params):
+        business_account = BusinessOwner.objects.filter(
+            id=params
+        ).values()
+        student_account = Student.objects.filter(
+            id=params
+        ).values()
+        admin_account = Administrator.objects.filter(
+            id=params
+        )
+        if business_account.count() > 0:
+            GeneralParams.field_token_rebase = business_account
+            return GeneralParams.field_token_rebase
+        elif student_account.count() > 0:
+            GeneralParams.field_token_rebase = student_account
+            return GeneralParams.field_token_rebase
+        elif admin_account.count() > 0:
+            GeneralParams.field_token_rebase = admin_account
+            return GeneralParams.field_token_rebase
+        else:
+            return GeneralParams.field_token_rebase
+
+
+class SystemDecryptor:
+    def decrypt(webpassword, hashpassword):
+        return check_password(webpassword, hashpassword)
+
+
+class SystemGenerator:
+    def job(size, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+
+class QueryBuilder:
+    def tokenizationQueryBuild(snapshot):
+        tokenization = Tokenization()
+        tokenization.userID = snapshot['userID']
+        tokenization.token = snapshot['token']
+        tokenization.lastRoute = snapshot['lastRoute']
+        tokenization.isDestroyed = "0"
+        tokenization.isvalid = "1"
+        tokenization.save()
+        GeneralParams.field_login_lastId = tokenization.id
+        GeneralParams.field_login_afterserializer = "success_login"
+        return GeneralParams.field_login_afterserializer
